85b5bf9ef4bd37423a05950242a2d085dd7f47c7
Александра Могильда <sashimogi@gmail.com>
2024-12-26 22:41:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+from collections import defaultdict
 
 def get_birthdays_per_week(users):
-    days_of_week = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
-    
     today = date.today()
-    
+    next_week = today + timedelta(days=7)
+    birthdays = defaultdict(list)
+
     for user in users:
-        birthday = user['birthday']
+        birthday = user["birthday"].replace(year=today.year)
+
+        if birthday < today:
+            birthday = user["birthday"].replace(year=today.year + 1)
         
-        if birthday.replace(year=today.year) < today:
-            birthday = birthday.replace(year=today.year + 1)
-        
-        day_of_week = birthday.weekday()
-        
-        day_names = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-        day_name = day_names[day_of_week]
-        
-        days_of_week[day_name].append(user['name'])
-    
-    return days_of_week
+        if today <= birthday < next_week:
+            day_name = birthday.strftime('%A')  
+            if birthday.weekday() >= 5:  
+                birthdays["Monday"].append(user["name"])
+            else:
+                birthdays[day_name].append(user["name"])
+
+    return birthdays
 
 if __name__ == "__main__":
     users = [
-        {"name": "Bill Gates", "birthday": datetime(1955, 10, 28).date()},
         {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
-        {"name": "Kim", "birthday": datetime(1990, 4, 15).date()},
+        {"name": "Bill Gates", "birthday": datetime(1955, 10, 28).date()},
+        {"name": "Mark Zuckerberg", "birthday": datetime(1984, 5, 14).date()},
+        {"name": "Elon Musk", "birthday": datetime(1971, 6, 28).date()},
+        {"name": "Jeff Bezos", "birthday": datetime(1964, 1, 12).date()},
     ]
-    
+
     result = get_birthdays_per_week(users)
     print(result)
-    
     for day_name, names in result.items():
         print(f"{day_name}: {', '.join(names)}")
